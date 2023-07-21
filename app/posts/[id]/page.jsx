ea3652618eb9a1708b266c89534ceb3d1141e0a3
Abdoallah Badr <98709917+Abdoallah-Badr@users.noreply.github.com>
2023-07-21 23:12:52 +0300

    edit fetching posts process

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
-import styles from "./loading.module.css";
 import Comments from "@/app/components/comments";
 import FetchPost from "@/app/components/fetchPost";
 import LoadingComponent from "./loading";
+import classes from "./postDetails.module.css";
 
 const PostDetails = async ({ params }) => {
   return (
     <main>
-      <div className="preview">
+      <div className={classes.postDetails}>
         <>
           <h2> Post </h2>
           <Suspense fallback={<LoadingComponent />}>
